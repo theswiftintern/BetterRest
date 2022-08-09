7d1509b2d109c620bc2ca630e7a8d996fbea4df3
theswiftintern <theswiftintern@gmail.com>
2022-08-09 10:21:56 +0800

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by RJ Tedoco on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
